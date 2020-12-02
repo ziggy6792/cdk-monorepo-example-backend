6d0db9b9eb12c58fb49d0b272df50cd64ab1011d
Simon <simonaverhoeven@gmail.com>
2020-12-02 11:55:05 +0800

    created first resolver

--- packages/lambda-gq-resolver/src/index.ts
+++ packages/lambda-gq-resolver/src/index.ts
@@ -1,7 +1,26 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable import/prefer-default-export */
 import 'source-map-support/register';
-import { commonFunctionExample } from '@danielblignaut/common-lambda-lib/dist/utils';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema, Resolver, Query } from 'type-graphql';
+
+@Resolver()
+class HelloResolver {
+  private recipesCollection: string[] = [];
+
+  @Query((returns) => [String])
+  async hello() {
+    return 'Hello World';
+  }
+}
 
 export const handler = async (event: any): Promise<any> => {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver],
+  });
+
+  const apolloServer = new ApolloServer({ schema });
+
   return {
     statusCode: 200,
     body: 'hello from lambda-gq-resolver',
