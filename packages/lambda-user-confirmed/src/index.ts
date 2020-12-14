cc892a4408c3a8d63baa3ab3b7a839bdf9b965fc
Simon <simonaverhoeven@gmail.com>
2020-12-14 14:48:47 +0800

    working with lifeomic/axios-fetch

--- packages/lambda-user-confirmed/src/index.ts
+++ packages/lambda-user-confirmed/src/index.ts
@@ -1,18 +1,17 @@
 /* eslint-disable import/prefer-default-export */
 import 'source-map-support/register';
 import { ApolloClient } from 'apollo-client';
-import { createHttpLink, HttpLink } from 'apollo-link-http';
-import url from 'url';
-import aws4 from 'aws4';
+import { createHttpLink } from 'apollo-link-http';
 
 import axios from 'axios';
 import { aws4Interceptor } from 'aws4-axios';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import gql from 'graphql-tag';
-import fetch from 'node-fetch';
+
+import { buildAxiosFetch } from '@lifeomic/axios-fetch';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { buildAxiosFetch } = require('@lifeomic/axios-fetch');
+// const { buildAxiosFetch } = require('@lifeomic/axios-fetch');
 
 const interceptor = aws4Interceptor(
   {
@@ -41,55 +40,18 @@ export const handler = async (event: any): Promise<any> => {
 
   console.log(logText);
 
-  const awsGraphqlFetch = (uri, options) => {
-    options = options || {};
-    const urlObject = url.parse(uri);
-
-    const signable = { host: urlObject.host, path: urlObject.path, headers: {} };
-
-    const list = ['method', 'body', 'headers', 'region', 'service'];
-
-    list.forEach((key) => {
-      signable[key] = options[key];
-    });
-
-    const credentials = {
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      sessionToken: process.env.AWS_SESSION_TOKEN,
-    };
-    aws4.sign(signable, credentials);
-    options.headers = signable.headers;
-    return fetch(uri, options);
-  };
   const client = new ApolloClient({
     link: createHttpLink({
       uri: 'https://mnakrqgp7b.execute-api.ap-southeast-1.amazonaws.com/dev/internal/graphql',
       fetch: buildAxiosFetch(axios),
-    }) as any,
+    }),
     cache: new InMemoryCache(),
   });
-
-  // const client = new ApolloClient({
-  //   link: new HttpLink({
-  //     uri: 'https://mnakrqgp7b.execute-api.ap-southeast-1.amazonaws.com/dev/internal/graphql',
-  //     fetch: awsGraphqlFetch,
-  //   }),
-  //   cache: new InMemoryCache(),
-  // });
 
   try {
     const response = await client.query({ query: HELLO });
 
-    // const response = await axios.post('https://mnakrqgp7b.execute-api.ap-southeast-1.amazonaws.com/dev/internal/graphql', {
-    //   query: `{
-    //     hello
-    //   }
-    //   `,
-    // });
-    console.log('response', response);
-
-    // client.query
+    console.log('response', response.data);
   } catch (err) {
     console.log({ err });
   }
