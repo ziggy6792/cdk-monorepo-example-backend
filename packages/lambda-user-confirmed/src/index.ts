bf0d3e4cd10fb989fa4eea201b3042e68146b850
Simon <simonaverhoeven@gmail.com>
2020-12-14 15:17:48 +0800

    removed access keys

--- packages/lambda-user-confirmed/src/index.ts
+++ packages/lambda-user-confirmed/src/index.ts
@@ -40,8 +40,8 @@ const REGISTER = gql`
 
 export const handler = async (event: any): Promise<any> => {
   const logText = `
-  AWS_ACCESS_KEY_ID = ${process.env.AWS_ACCESS_KEY_ID};
-  AWS_SECRET_ACCESS_KEY = ${process.env.AWS_SECRET_ACCESS_KEY};
+  AWS_ACCESS_KEY_ID = ${process.env.AWS_ACCESS_KEY_ID}
+  AWS_SECRET_ACCESS_KEY = ${process.env.AWS_SECRET_ACCESS_KEY}
   AWS_SESSION_TOKEN = ${process.env.AWS_SESSION_TOKEN}`;
 
   console.log(logText);
