d83000380ca269868e7ff6b597e25066c32cfb74
Simon <simonaverhoeven@gmail.com>
2021-01-03 12:50:41 +0800

    test master commit

--- packages/lambda-gq-resolver/src/config/index.ts
+++ packages/lambda-gq-resolver/src/config/index.ts
@@ -3,6 +3,6 @@ export const REGION = process.env.REGION || 'ap-southeast-1';
 export const ENV = process.env.ENV || 'dev';
 export const COGNITO_USER_POOL_ID = process.env.COGNITO_USER_POOL_ID || 'ap-southeast-1_eLTcaObre';
 
-// Others
+// Other
 export const IS_PROD = ENV === 'prod';
 export const TABLE_NAME_PREFIX = IS_PROD ? 'cdk-monorepo-prod-' : 'cdk-monorepo-dev-';
