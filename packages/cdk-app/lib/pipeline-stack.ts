054e8b6a0be0573a9384da0b5959fd83081a4768
Simon <simonaverhoeven@gmail.com>
2021-01-05 15:22:38 +0800

    change repo name

--- packages/cdk-app/lib/pipeline-stack.ts
+++ packages/cdk-app/lib/pipeline-stack.ts
@@ -31,7 +31,7 @@ class PipelineStack extends Stack {
         trigger: codepipelineActions.GitHubTrigger.POLL,
         // Replace these with your actual GitHub project info
         owner: 'ziggy6792',
-        repo: 'cdk-monorepo-example',
+        repo: 'cdk-monorepo-example-backend',
         branch: 'feature/add-pipeline-2',
       }),
 
