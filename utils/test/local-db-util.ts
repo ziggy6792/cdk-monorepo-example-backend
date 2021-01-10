566838a09e05ef3e658a32ac0973a714c0eda583
Simon <simonaverhoeven@gmail.com>
2021-01-10 15:04:38 +0800

    setup and teardown done

--- utils/test/local-db-util.ts
+++ utils/test/local-db-util.ts
@@ -1,13 +1,39 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import * as child from 'child_process';
 import kill from 'tree-kill';
+import poll from 'promise-poller';
+import util from 'util';
 
-const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+const exec = util.promisify(child.exec);
 
 let localDb: child.ChildProcessWithoutNullStreams;
 
+export const checkConnection = async (): Promise<boolean> => {
+  try {
+    const { stderr } = await exec('nc -z localhost 8000 ');
+    console.log(stderr);
+    const isReady = stderr.includes('Connection to localhost port 8000 [tcp/irdmi] succeeded!');
+    return isReady;
+  } catch (err) {
+    // console.log('Local db not ready...');
+    throw new Error(err);
+  }
+};
+
 export const start = async (): Promise<void> => {
-  console.log('start');
+  console.log('\n\nLOCAL DB: INIT');
+  try {
+    const isAlredyRunning = await checkConnection();
+    if (isAlredyRunning) {
+      // Just incase the server is somehow stil running from the last run
+      console.log('LOCAL DB: FORCE STOP');
+      const { stderr } = await exec('lsof -ti tcp:8000 | xargs kill');
+    }
+  } catch (err) {
+    // Do nothing - must be is it not running
+  }
+
+  console.log('LOCAL DB: START');
 
   localDb = child.spawn('yarn', ['start:local:db']);
 
@@ -23,10 +49,19 @@ export const start = async (): Promise<void> => {
     console.log(`child process exited with code ${code.toString()}`);
   });
 
-  await delay(3000);
+  try {
+    await poll({
+      taskFn: checkConnection,
+      interval: 100,
+      retries: 200,
+    });
+  } catch (err) {
+    console.log('Could not connect to local db');
+  }
 };
 
 export const stop = (): void => {
-  console.log('stop');
+  console.log('\n\nLOCAL DB: STOP');
+
   kill(localDb.pid);
 };
