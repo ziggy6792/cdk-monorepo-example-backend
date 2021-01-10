4ae186a83b93648ea723a93e6eceb3ef11e6e6f6
Simon <simonaverhoeven@gmail.com>
2021-01-10 18:37:13 +0800

    updated setup

--- utils/test/local-db-util.ts
+++ utils/test/local-db-util.ts
@@ -3,40 +3,69 @@ import * as child from 'child_process';
 import kill from 'tree-kill';
 import poll from 'promise-poller';
 import util from 'util';
+import { ServiceConfigurationOptions } from 'aws-sdk/lib/service';
+import AWS = require('aws-sdk');
 
 const exec = util.promisify(child.exec);
 
 let localDb: child.ChildProcessWithoutNullStreams;
 
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
 // export const checkConnection = async (): Promise<boolean> => {
-// Removed because aws does not have nc instaled
+//   // Removed because aws does not have nc instaled
 //   try {
 //     const { stderr } = await exec('nc -z localhost 8000 ');
 //     console.log(stderr);
 //     const isReady = stderr.includes('Connection to localhost port 8000 [tcp/irdmi] succeeded!');
 //     return isReady;
 //   } catch (err) {
-//     console.log('LOCAL DB: connection error');
-//     console.log(err);
+//     // console.log('LOCAL DB: connection error');
 //     throw new Error(err);
 //   }
 // };
 
+// export const checkConnection = async (): Promise<boolean> => {
+//   let isReady = false;
+//   try {
+//     const { stderr } = await exec('bash -c "</dev/tcp/localhost/8000"; echo $?');
+//     console.log('LOCAL DB: ', stderr);
+//     isReady = !stderr.includes('Connection refused');
+//   } catch (err) {
+//     console.log('LOCAL DB: connection error');
+//     console.log(err);
+//     throw new Error(err);
+//   }
+//   if (!isReady) {
+//     throw new Error('Not ready');
+//   }
+//   console.log('LOCAL DB: Connection Ready');
+//   return true;
+// };
+
+const promiseWithTimeout = function (promise: Promise<any>, ms: number) {
+  // Create a promise that rejects in <ms> milliseconds
+  const timeout = new Promise((resolve, reject) => {
+    const id = setTimeout(() => {
+      clearTimeout(id);
+      reject(new Error(`Timed out in ${ms}ms.`));
+    }, ms);
+  });
+
+  // Returns a race between our timeout and the passed in promise
+  return Promise.race([promise, timeout]);
+};
+
 export const checkConnection = async (): Promise<boolean> => {
-  let isReady = false;
-  try {
-    const { stderr } = await exec('bash -c "</dev/tcp/localhost/8000"; echo $?');
-    console.log('LOCAL DB: ', stderr);
-    isReady = !stderr.includes('Connection refused');
-  } catch (err) {
-    console.log('LOCAL DB: connection error');
-    console.log(err);
-    throw new Error(err);
-  }
-  if (!isReady) {
-    throw new Error('Not ready');
-  }
-  console.log('LOCAL DB: Connection Ready');
+  const serviceConfigOptions: ServiceConfigurationOptions = {
+    region: 'local',
+    endpoint: 'http://localhost:8000',
+  };
+
+  const dynamodb = new AWS.DynamoDB(serviceConfigOptions);
+  const tables = await promiseWithTimeout(dynamodb.listTables().promise(), 100);
+  console.log('LOCAL DB: LOCAL DB READY');
+
   return true;
 };
 
