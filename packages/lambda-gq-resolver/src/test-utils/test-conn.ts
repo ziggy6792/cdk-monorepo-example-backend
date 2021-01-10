b59e97eccd0f3b1f6250e1a04079496d728dfb77
Simon <simonaverhoeven@gmail.com>
2021-01-10 15:07:57 +0800

    remove old test setup

--- packages/lambda-gq-resolver/src/test-utils/test-conn.ts
+++ packages/lambda-gq-resolver/src/test-utils/test-conn.ts
@@ -1,8 +1,8 @@
 import AWS from 'aws-sdk';
 import { ServiceConfigurationOptions } from 'aws-sdk/lib/service';
-import { deleteTables, initMapper, initTables } from '../utils/mapper';
+import { initMapper, initTables } from '../utils/mapper';
 
-const testConn = async (dropAll = false): Promise<void> => {
+const testConn = async (): Promise<void> => {
   const serviceConfigOptions: ServiceConfigurationOptions = {
     region: 'local',
     endpoint: 'http://localhost:8000',
@@ -12,9 +12,9 @@ const testConn = async (dropAll = false): Promise<void> => {
 
   initMapper({ region: 'local', tableNamePrefix: 'test-' });
 
-  if (dropAll) {
-    await deleteTables();
-  }
+  // if (dropAll) {
+  //   await deleteTables();
+  // }
   await initTables();
 };
 
