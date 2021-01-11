3b60e98574f6dd929e81f9526acb9ba346ab48eb
Simon <simonaverhoeven@gmail.com>
2021-01-11 20:04:42 +0800

    moved local-server to utils

--- packages/lambda-a/src/index.ts
+++ packages/lambda-a/src/index.ts
@@ -5,7 +5,7 @@ import example from 'src/example';
 
 export const handler = async (event: any): Promise<any> => {
   example();
-  console.log('bla bla');
+  console.log('bla');
   return {
     statusCode: 200,
     body: JSON.stringify({
