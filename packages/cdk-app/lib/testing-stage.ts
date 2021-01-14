843ca25593eb8b75da48616e27d8f851c4cc6a37
Simon <simonaverhoeven@gmail.com>
2021-01-15 02:53:20 +0800

    cleanup

--- packages/cdk-app/lib/testing-stage.ts
+++ packages/cdk-app/lib/testing-stage.ts
@@ -1,21 +1,18 @@
 /* eslint-disable import/prefer-default-export */
-import { Construct, Stage, StageProps } from '@aws-cdk/core';
 
 import * as cdk from '@aws-cdk/core';
-import * as defaults from '@aws-solutions-constructs/core';
-import * as path from 'path';
+import { DEPLOYMENT_CONFIG } from 'config';
 import { DeploymentStack } from './deployment-stack';
-import { DEPLOYMENT_CONFIG } from '../config';
 
 export interface DeploymentStageProps extends cdk.StackProps {
   readonly stageName: string;
 }
 
-export class DeploymentStage extends Stage {
+export class DeploymentStage extends cdk.Stage {
   public readonly urlOutput: cdk.CfnOutput;
   // public stack: ApigwDemoStack;
 
-  constructor(scope: Construct, id: string, props?: DeploymentStageProps) {
+  constructor(scope: cdk.Construct, id: string, props?: DeploymentStageProps) {
     super(scope, id, props);
 
     const { stageName } = props;
