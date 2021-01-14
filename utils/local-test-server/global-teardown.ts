9c959bfeb5a9414d1786a5d3980fc45b2722536a
Simon <simonaverhoeven@gmail.com>
2021-01-15 00:43:51 +0800

    cleanup

--- utils/local-test-server/global-teardown.ts
+++
@@ -1,9 +0,0 @@
-import { stop } from './local-db-util';
-
-// const delay = (ms) => new Promise((res) => setTimeout(res, ms));
-
-const tearDown = async (): Promise<void> => {
-  stop();
-};
-
-export default tearDown;
