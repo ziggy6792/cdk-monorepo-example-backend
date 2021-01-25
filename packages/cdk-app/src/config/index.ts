ec79750b9352d2cbaa36086743145dc36b6118f2
Simon <simonaverhoeven@gmail.com>
2021-01-25 11:46:45 +0800

    old account

--- packages/cdk-app/src/config/index.ts
+++ packages/cdk-app/src/config/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 export const PROJECT_NAME = 'cdk-monorepo-backend';
-export const AWS_ACCOUNT_ID = '932244219675';
+export const AWS_ACCOUNT_ID = '694710432912';
 export const AWS_REGION = 'ap-southeast-1';
 
 export const DEPLOYMENT_CONFIG = {
