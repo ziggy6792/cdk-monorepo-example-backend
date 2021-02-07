d8ff85654fad7b989a4e81c2d974e0919f31356f
Simon Verhoeven <SimonAndrew.Verhoeven@sc.com>
2021-02-07 15:54:10 +0800

    workaound 2

--- packages/cdk-app/src/index.ts
+++ packages/cdk-app/src/index.ts
@@ -5,32 +5,34 @@ import PipelineStack from 'src/stacks/pipeline-stack';
 import createDummyStack from 'src/dummy-stacks/dummy-deployment-stack';
 import LocalTestStack from 'src/local-stacks/local-test-stack';
 
-const app = new cdk.App();
+const app = new cdk.App({ context: { custom: 'bla bla bla' } });
+
+// console.log('context', app.node.tryGetContext('custom'));
+// console.log('context', app.node.tryGetContext('@aws-cdk/core:newStyleStackSynthesis'));
+
+// console.log(JSON.parse(process.env.npm_config_argv));
+
+// console.log(JSON.parse(process.env.CDK_CONTEXT_JSON));
 
 // Dummy stack
 createDummyStack(app);
 
-const testStack = new LocalTestStack(app, utils.getConstructId('local-test'), {
-    stageName: 'test',
-});
-
-console.log('context', app.node.tryGetContext('custom'));
-console.log('context', app.node.tryGetContext('@aws-cdk/core:newStyleStackSynthesis'));
-
-console.log(JSON.parse(process.env.npm_config_argv));
-
 const context = JSON.parse(process.env.CDK_CONTEXT_JSON);
-
-console.log(JSON.parse(process.env.CDK_CONTEXT_JSON));
 
 if (context['@aws-cdk/core:newStyleStackSynthesis']) {
     // eslint-disable-next-line no-constant-condition
     const pipelineStack = new PipelineStack(app, utils.getConstructId('pipeline'), {
+        stackName: 'stack1',
         description: utils.getConstructId('pipeline'),
         env: {
             account: config.AWS_ACCOUNT_ID,
             region: config.AWS_REGION,
         },
+    });
+} else {
+    const testStack = new LocalTestStack(app, utils.getConstructId('local-test'), {
+        stackName: 'stack2',
+        stageName: 'test',
     });
 }
 
