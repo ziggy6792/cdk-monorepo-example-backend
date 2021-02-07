cb135862bb7b2411c31fdc69270cfbd369bb001a
Simon Verhoeven <SimonAndrew.Verhoeven@sc.com>
2021-02-07 23:55:50 +0800

    switched pipeline branch

--- packages/cdk-app/src/stacks/pipeline-stack/pipeline-stack.ts
+++ packages/cdk-app/src/stacks/pipeline-stack/pipeline-stack.ts
@@ -35,7 +35,7 @@ class PipelineStack extends cdk.Stack {
                 // Replace these with your actual GitHub project info
                 owner: 'ziggy6792',
                 repo: 'cdk-monorepo-example-backend',
-                branch: 'master',
+                branch: 'feature/new-test-env',
             }),
 
             synthAction: cdkPipeline.SimpleSynthAction.standardYarnSynth({
