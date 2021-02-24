6346f452dfff2e65ced7eccb628fcb8c46567eed
simon-verhoeven <simonaverhoeven@gmail.com>
2021-02-24 15:07:10 +0800

    switched pipeline

--- packages/cdk-app/src/stacks/pipeline-stack/pipeline-stack.ts
+++ packages/cdk-app/src/stacks/pipeline-stack/pipeline-stack.ts
@@ -35,7 +35,7 @@ class PipelineStack extends cdk.Stack {
                 // Replace these with your actual GitHub project info
                 owner: 'ziggy6792',
                 repo: 'cdk-monorepo-example-backend',
-                branch: 'master',
+                branch: 'update/use-aws-docker-gallery',
             }),
 
             synthAction: cdkPipeline.SimpleSynthAction.standardYarnSynth({
