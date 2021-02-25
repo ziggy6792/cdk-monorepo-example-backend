9227e89976984aa1863bdc919373bfcc728400be
simon-verhoeven <simonaverhoeven@gmail.com>
2021-02-26 01:40:04 +0800

    update facebook app

--- packages/cdk-app/src/config/index.ts
+++ packages/cdk-app/src/config/index.ts
@@ -6,8 +6,8 @@ export const AWS_ACCOUNT_ID = '932244219675';
 export const DEPLOYMENT_CONFIG = {
     staging: {
         domainPrefix: `${commonConfig.PROJECT_NAME}-staging`,
-        facebookClientId: '2655230284789320',
-        facebookClientSecret: '735671e92e39d55958f27e3def6e6e65',
+        facebookClientId: '401988904382290',
+        facebookClientSecret: '56dc78be341d68d0f0e3229a6ee37723',
     },
     prod: {
         domainPrefix: `${commonConfig.PROJECT_NAME}-prod`,
@@ -16,7 +16,7 @@ export const DEPLOYMENT_CONFIG = {
     },
     dummy: {
         domainPrefix: `${commonConfig.PROJECT_NAME}-dummy`,
-        facebookClientId: '153415723229752',
-        facebookClientSecret: '8d373404c59caf111fe055f6e9deeb15',
+        facebookClientId: '401988904382290',
+        facebookClientSecret: '56dc78be341d68d0f0e3229a6ee37723',
     },
 };
