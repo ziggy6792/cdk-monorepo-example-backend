1cb9c5b38db59d59ab08849fd7f036105e93ad9f
simon-verhoeven <simonaverhoeven@gmail.com>
2021-03-02 22:34:10 +0800

    added score run inputs

--- packages/lambda-gq-resolver/src/modules/score-run/inputs/score-run-inputs.ts
+++ packages/lambda-gq-resolver/src/modules/score-run/inputs/score-run-inputs.ts
@@ -1,8 +1,17 @@
 /* eslint-disable max-classes-per-file */
 /* eslint-disable import/prefer-default-export */
-import { Field, InputType, ID, Int } from 'type-graphql';
+import { Field, InputType, ID, Int, Float } from 'type-graphql';
 import { UpdateRiderAllocationInput } from 'src/modules/crud/rider-allocation/inputs/rider-allocation-inputs';
 import { Run } from 'src/domain/models/rider-allocation';
+
+@InputType()
+export class RunInput {
+    @Field(() => Float, { nullable: true })
+    score: number;
+
+    @Field({ nullable: true })
+    isPublic: boolean;
+}
 
 @InputType()
 export class ScorRunInput {
@@ -12,6 +21,6 @@ export class ScorRunInput {
     @Field(() => ID)
     userId: string;
 
-    // @Field(() => [Run])
-    // runs: [Run];
+    @Field(() => [RunInput])
+    runs: [RunInput];
 }
