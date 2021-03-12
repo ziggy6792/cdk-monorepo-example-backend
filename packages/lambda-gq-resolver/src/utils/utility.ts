9012dee76492f52e32e58bf0f9e0c8624c063d9a
simon-verhoeven <simonaverhoeven@gmail.com>
2021-03-12 12:42:42 +0800

    remove unused packages

--- packages/lambda-gq-resolver/src/utils/utility.ts
+++ packages/lambda-gq-resolver/src/utils/utility.ts
@@ -1,5 +1,3 @@
-import { FunctionExpression, AttributePath, ConditionExpression } from '@aws/dynamodb-expressions';
-
 import getEnvConfig from 'src/config/get-env-config';
 import { commonUtils } from '@alpaca-backend/common';
 
@@ -14,22 +12,6 @@ export const valueIsNull = (value: any): boolean => value === null || value === 
 export const getTableName = (tableName: string): string => {
     const ret = commonUtils.getTableName(tableName, envConig.env);
     return ret;
-};
-
-export const createNotExistsCondition = (attributePaths = ['id']): ConditionExpression => {
-    const andExpression: ConditionExpression = {
-        type: 'And',
-        conditions: attributePaths.map((path) => new FunctionExpression('attribute_not_exists', new AttributePath(path))),
-    };
-    return andExpression;
-};
-
-export const createExistsCondition = (attributePaths = ['id']): ConditionExpression => {
-    const andExpression: ConditionExpression = {
-        type: 'And',
-        conditions: attributePaths.map((path) => new FunctionExpression('attribute_exists', new AttributePath(path))),
-    };
-    return andExpression;
 };
 
 export const mapDbException = (err: any, message: string): any => {
